7695654dafcdce73df2e2f8d8a5082115de52e0a
Phan Minh Tuấn <127879091+TuanPhan282@users.noreply.github.com>
2024-11-06 22:12:36 +0700

    done list question, answer question (#33)

--- helpers/api/exam-api.ts
+++ helpers/api/exam-api.ts
@@ -21,275 +21,314 @@ import { BASE_URL } from "../../constants";
 // };
 
 const getAssignedExams = async (userId: string, token: string) => {
-  // TODO: handle res.json() error when response not in json
+    // TODO: handle res.json() error when response not in json
 
-  try {
-    const res = await fetch(`${BASE_URL}/${userId}/assignedExams/all`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    try {
+        const res = await fetch(`${BASE_URL}/${userId}/assignedExams/all`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.err) {
-      throw new Error(data.err || "Failed to get assigned exams from server!");
+        if (!res.ok || data.err) {
+            throw new Error(
+                data.err || "Failed to get assigned exams from server!"
+            );
+        }
+
+        return data.exams;
+    } catch (e) {
+        throw e;
     }
-
-    return data.exams;
-  } catch (e) {
-    throw e;
-  }
 };
 
 const getExam = async (id: string, accessToken: string, examId: string) => {
-  try {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id);
-    myHeaders.append("Content-Type", "application/json");
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id);
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(`${BASE_URL}/exam/${examId}`, {
-      method: "GET",
-      headers: myHeaders,
-    });
+        const res = await fetch(`${BASE_URL}/exam/${examId}`, {
+            method: "GET",
+            headers: myHeaders,
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to get exam!");
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to get exam!");
+        }
+        return data.metadata;
+    } catch (e) {
+        throw new Error(e.message || "Failed to get exam!");
     }
-    return data.metadata;
-  } catch (e) {
-    throw new Error(e.message || "Failed to get exam!");
-  }
 };
 
 const getListExam = async (
-  id: string,
-  accessToken: string,
-  page: number,
-  limit: number
+    id: string,
+    accessToken: string,
+    page: number,
+    limit: number
 ) => {
-  try {
-    console.log("User ID:", id);
-    console.log("Access Token:", accessToken);
+    try {
+        console.log("User ID:", id);
+        console.log("Access Token:", accessToken);
 
-    // Create a Headers object
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken); // Use the access token directly
-    myHeaders.append("x-client-id", id || ""); // Use the user ID as the client ID
+        // Create a Headers object
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken); // Use the access token directly
+        myHeaders.append("x-client-id", id || ""); // Use the user ID as the client ID
 
-    const res = await fetch(
-      `${BASE_URL}/exam/list?page=${page}&limit=${limit}`,
-      {
-        method: "GET",
-        headers: myHeaders, // Use the Headers object
-      }
-    );
+        const res = await fetch(
+            `${BASE_URL}/exam/list?page=${page}&limit=${limit}`,
+            {
+                method: "GET",
+                headers: myHeaders, // Use the Headers object
+            }
+        );
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to get list!");
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to get list!");
+        }
+
+        return data.metadata.exams;
+    } catch (e) {
+        throw new Error(e.message || "Failed to get list exam!");
     }
+};
 
-    return data.metadata.exams;
-  } catch (e) {
-    throw new Error(e.message || "Failed to get list exam!");
-  }
+const getListQuestion = async (
+    id: string,
+    accessToken: string,
+    examId: string,
+    page: number,
+    limit: number
+) => {
+    try {
+        console.log("User ID:", id);
+        console.log("Access Token:", accessToken);
+
+        // Create a Headers object
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken); // Use the access token directly
+        myHeaders.append("x-client-id", id || ""); // Use the user ID as the client ID
+
+        const res = await fetch(
+            `${BASE_URL}/question/${examId}/list?page=${page}&limit=${limit}`,
+            {
+                method: "GET",
+                headers: myHeaders, // Use the Headers object
+            }
+        );
+
+        const data = await res.json();
+
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to get list!");
+        }
+
+        return data.metadata;
+    } catch (e) {
+        throw new Error(e.message || "Failed to get list question!");
+    }
 };
 
 const searchExam = async (id: string, accessToken: string, query: string) => {
-  try {
-    console.log("User ID:", id);
-    console.log("Access Token:", accessToken);
-    console.log("Search Query:", query);
+    try {
+        console.log("User ID:", id);
+        console.log("Access Token:", accessToken);
+        console.log("Search Query:", query);
 
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id || "");
-    myHeaders.append("Content-Type", "application/json");
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id || "");
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(
-      `${BASE_URL}/exam/search?query=${encodeURIComponent(query)}`,
-      {
-        method: "GET",
-        headers: myHeaders,
-      }
-    );
+        const res = await fetch(
+            `${BASE_URL}/exam/search?query=${encodeURIComponent(query)}`,
+            {
+                method: "GET",
+                headers: myHeaders,
+            }
+        );
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to search exam!");
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to search exam!");
+        }
+
+        return data.metadata.exams;
+    } catch (e) {
+        throw new Error(e.message || "Failed to search exam!");
     }
-
-    return data.metadata.exams;
-  } catch (e) {
-    throw new Error(e.message || "Failed to search exam!");
-  }
 };
 
 const deleteExam = async (
-  id: string,
-  accessToken: string,
-  examId: string,
-  examData: any
+    id: string,
+    accessToken: string,
+    examId: string,
+    examData: any
 ) => {
-  try {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id);
-    myHeaders.append("Content-Type", "application/json");
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id);
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(`${BASE_URL}/exam/${examId}`, {
-      method: "DELETE",
-      headers: myHeaders,
-      body: JSON.stringify(examData),
-    });
+        const res = await fetch(`${BASE_URL}/exam/${examId}`, {
+            method: "DELETE",
+            headers: myHeaders,
+            body: JSON.stringify(examData),
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to delete exam!");
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to delete exam!");
+        }
+        return data.metadata;
+    } catch (e) {
+        throw new Error(e.message || "Failed to delete exam!");
     }
-    return data.metadata;
-  } catch (e) {
-    throw new Error(e.message || "Failed to delete exam!");
-  }
 };
 
 const createExam = async (id: string, accessToken: string, exam: any) => {
-  try {
-    console.log("Creating user with ID:", id);
-    console.log("Access Token:", accessToken);
+    try {
+        console.log("Creating user with ID:", id);
+        console.log("Access Token:", accessToken);
 
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id);
-    myHeaders.append("Content-Type", "application/json");
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id);
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(`${BASE_URL}/exam/create`, {
-      method: "POST",
-      headers: myHeaders,
-      body: JSON.stringify(exam),
-    });
+        const res = await fetch(`${BASE_URL}/exam/create`, {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify(exam),
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok) {
-      throw new Error(data.message || "Failed to create exam!");
+        if (!res.ok) {
+            throw new Error(data.message || "Failed to create exam!");
+        }
+
+        return data.metadata;
+    } catch (e) {
+        console.error("Error in createExam:", e);
+        throw e;
     }
-
-    return data.metadata;
-  } catch (e) {
-    console.error("Error in createExam:", e);
-    throw e;
-  }
 };
 
 const updateExam = async (
-  id: string,
-  accessToken: string,
-  examId: string,
-  examData: any
+    id: string,
+    accessToken: string,
+    examId: string,
+    examData: any
 ) => {
-  try {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id);
-    myHeaders.append("Content-Type", "application/json");
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id);
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(`${BASE_URL}/exam/${examId}`, {
-      method: "PATCH",
-      headers: myHeaders,
-      body: JSON.stringify(examData),
-    });
+        const res = await fetch(`${BASE_URL}/exam/${examId}`, {
+            method: "PATCH",
+            headers: myHeaders,
+            body: JSON.stringify(examData),
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to update exam!");
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to update exam!");
+        }
+        return data.metadata;
+    } catch (e) {
+        throw new Error(e.message || "Failed to update exam!");
     }
-    return data.metadata;
-  } catch (e) {
-    throw new Error(e.message || "Failed to update exam!");
-  }
 };
 
 const createQuestion = async (
-  id: string,
-  accessToken: string,
-  examId: string,
-  questionData: any
+    id: string,
+    accessToken: string,
+    examId: string,
+    questionData: any
 ) => {
-  try {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id); // Thêm dòng này để sử dụng id làm client ID
-    myHeaders.append("Content-Type", "application/json");
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id); // Thêm dòng này để sử dụng id làm client ID
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(`${BASE_URL}/question/${examId}/create`, {
-      method: "POST",
-      headers: myHeaders,
-      body: JSON.stringify(questionData),
-    });
+        const res = await fetch(`${BASE_URL}/question/${examId}/create`, {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify(questionData),
+        });
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to create question!");
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to create question!");
+        }
+        return data.metadata;
+    } catch (e) {
+        throw new Error(e.message || "Failed to create question!");
     }
-    return data.metadata;
-  } catch (e) {
-    throw new Error(e.message || "Failed to create question!");
-  }
 };
 
 const getExamsByStatus = async (
-  id: string,
-  accessToken: string,
-  status: string,
-  page: number,
-  limit: number
+    id: string,
+    accessToken: string,
+    status: string,
+    page: number,
+    limit: number
 ) => {
-  try {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id || "");
-    myHeaders.append("Content-Type", "application/json");
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id || "");
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(
-      `${BASE_URL}/exam/list?status=${encodeURIComponent(
-        status
-      )}&page=${page}&limit=${limit}`,
-      {
-        method: "GET",
-        headers: myHeaders,
-      }
-    );
+        const res = await fetch(
+            `${BASE_URL}/exam/list?status=${encodeURIComponent(
+                status
+            )}&page=${page}&limit=${limit}`,
+            {
+                method: "GET",
+                headers: myHeaders,
+            }
+        );
 
-    const data = await res.json();
+        const data = await res.json();
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to get exams by status!");
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to get exams by status!");
+        }
+
+        return data.metadata.exams;
+    } catch (e) {
+        throw new Error(e.message || "Failed to get exams by status!");
     }
-
-    return data.metadata.exams;
-  } catch (e) {
-    throw new Error(e.message || "Failed to get exams by status!");
-  }
 };
 
 export {
-  getExam,
-  getAssignedExams,
-  searchExam,
-  getListExam,
-  deleteExam,
-  createExam,
-  updateExam,
-  createQuestion,
-  getExamsByStatus,
+    getExam,
+    getAssignedExams,
+    searchExam,
+    getListExam,
+    deleteExam,
+    createExam,
+    updateExam,
+    createQuestion,
+    getExamsByStatus,
+    getListQuestion,
 };
