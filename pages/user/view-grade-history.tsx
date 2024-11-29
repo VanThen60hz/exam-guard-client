70e2fea547ea83ba8aaf7892661c071ebfeac16b
dwgpie <dungphan.170603@gmail.com>
2024-11-29 23:22:48 +0700

    add view grade history

---
+++ pages/user/view-grade-history.tsx
@@ -0,0 +1,20 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import { useRef } from "react";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
+import NavBarHome from "../../components/home/navbar-home";
+import ViewGradeHistoryForm from "../../components/user/view-grade-history";
+
+const ViewGradeHistory= () => {
+  const loadingBarRef: React.Ref<LoadingBarRef> = useRef(null);
+
+  return (
+    <div>
+      <NavBarHome loadingBarRef={loadingBarRef} />
+      <ViewGradeHistoryForm />
+    </div>
+  );
+};
+
+export default ViewGradeHistory;
