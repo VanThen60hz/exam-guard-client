a2de5ead80f17afbd49334d21c86c26ca76fbc0b
Phan Minh Tuấn <127879091+TuanPhan282@users.noreply.github.com>
2024-12-05 18:37:36 +0700

    toast - console (#71)

--- helpers/api/cheating-api.ts
+++ helpers/api/cheating-api.ts
@@ -1,106 +1,107 @@
 import { BASE_URL } from "../../constants";
 
 const detect_cheating = async (
-  id: string,
-  accessToken: string,
-  examId: string,
-  detect: any
+    id: string,
+    accessToken: string,
+    examId: string,
+    detect: any
 ) => {
-  try {
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken);
-    myHeaders.append("x-client-id", id);
-    myHeaders.append("Content-Type", "application/json");
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken);
+        myHeaders.append("x-client-id", id);
+        myHeaders.append("Content-Type", "application/json");
 
-    const res = await fetch(`${BASE_URL}/cheating/detect-cheating/${examId}`, {
-      method: "POST",
-      headers: myHeaders,
-      body: JSON.stringify(detect),
-    });
-    const data = await res.json();
+        const res = await fetch(
+            `${BASE_URL}/cheating/detect-cheating/${examId}`,
+            {
+                method: "POST",
+                headers: myHeaders,
+                body: JSON.stringify(detect),
+            }
+        );
+        console.log("Detect cheating successfully");
 
-    if (!res.ok) {
-      throw new Error(data.message || "Failed to cheating");
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(data.message || "Failed to cheating");
+        }
+
+        return data.metadata;
+    } catch (e) {
+        console.error("Failed to cheating:", e);
+        throw e;
     }
-
-    return data.metadata;
-  } catch (e) {
-    console.error("Failed to cheating:", e);
-    throw e;
-  }
 };
 
 const getListCheatingStatistic = async (
-  id: string,
-  accessToken: string,
-  examId: string,
-  page: number,
-  limit: number
+    id: string,
+    accessToken: string,
+    examId: string,
+    page: number,
+    limit: number
 ) => {
-  try {
-    console.log("User ID:", id);
-    console.log("Access Token:", accessToken);
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken); // Use the access token directly
+        myHeaders.append("x-client-id", id || ""); // Use the user ID as the client ID
 
-    // Create a Headers object
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken); // Use the access token directly
-    myHeaders.append("x-client-id", id || ""); // Use the user ID as the client ID
+        const res = await fetch(
+            `${BASE_URL}/cheating/list-cheating-statistics/${examId}?page=${page}&limit=${limit}`,
+            {
+                method: "GET",
+                headers: myHeaders,
+            }
+        );
+        console.log("Get statistic list successfully");
 
-    const res = await fetch(
-      `${BASE_URL}/cheating/list-cheating-statistics/${examId}?page=${page}&limit=${limit}`,
-      {
-        method: "GET",
-        headers: myHeaders, // Use the Headers object
-      }
-    );
+        const data = await res.json();
 
-    const data = await res.json();
+        if (!res.ok || data.status !== 200) {
+            throw new Error(data.message || "Failed to get list cheating");
+        }
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to get list cheating");
+        return data.metadata;
+    } catch (e) {
+        throw new Error(e.message || "Failed to get list cheating");
     }
-
-    return data.metadata;
-  } catch (e) {
-    throw new Error(e.message || "Failed to get list cheating");
-  }
 };
 
 const getListCheatingByStudent = async (
-  id: string,
-  accessToken: string,
-  examId: string,
-  studentId: string,
-  page,
-  limit
+    id: string,
+    accessToken: string,
+    examId: string,
+    studentId: string,
+    page,
+    limit
 ) => {
-  try {
-    console.log("User ID:", id);
-    console.log("Access Token:", accessToken);
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", accessToken); // Use the access token directly
+        myHeaders.append("x-client-id", id || ""); // Use the user ID as the client ID
 
-    // Create a Headers object
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", accessToken); // Use the access token directly
-    myHeaders.append("x-client-id", id || ""); // Use the user ID as the client ID
+        const res = await fetch(
+            `${BASE_URL}/cheating/list-cheating-histories-by-student/${examId}/${studentId}?page=${page}&limit=${limit}`,
+            {
+                method: "GET",
+                headers: myHeaders, // Use the Headers object
+            }
+        );
+        console.log("Get cheating list successfully");
 
-    const res = await fetch(
-      `${BASE_URL}/cheating/list-cheating-histories-by-student/${examId}/${studentId}?page=${page}&limit=${limit}`,
-      {
-        method: "GET",
-        headers: myHeaders, // Use the Headers object
-      }
-    );
+        const data = await res.json();
 
-    const data = await res.json();
+        if (!res.ok || data.status !== 200) {
+            throw new Error(
+                data.message || "Failed to get list cheating by student"
+            );
+        }
 
-    if (!res.ok || data.status !== 200) {
-      throw new Error(data.message || "Failed to get list cheating by student");
+        return data.metadata;
+    } catch (e) {
+        throw new Error(e.message || "Failed to get list cheating by student");
     }
-
-    return data.metadata;
-  } catch (e) {
-    throw new Error(e.message || "Failed to get list cheating by student");
-  }
 };
 
 export { detect_cheating, getListCheatingStatistic, getListCheatingByStudent };
